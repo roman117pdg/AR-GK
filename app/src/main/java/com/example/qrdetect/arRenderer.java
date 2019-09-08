660fd2f6bbe63ba2ee6dc7889fa6c38a3ab0aebe
Lejdi <lejdi1618@gmail.com>
2019-09-08 15:29:02 +0200

    jakotako

--- app/src/main/java/com/example/qrdetect/arRenderer.java
+++ app/src/main/java/com/example/qrdetect/arRenderer.java
@@ -1,6 +1,5 @@
 package com.example.qrdetect;
 
-import android.content.Context;
 import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
@@ -12,7 +11,6 @@ public class arRenderer implements GLSurfaceView.Renderer {
     private int mWidth;
     private int mHeight;
 
-    private float mAngle =0;
     private float mTransY=0;
     private float mTransX=0;
 
@@ -25,7 +23,7 @@ public class arRenderer implements GLSurfaceView.Renderer {
     private final float[] mRotationMatrix = new float[16];
 
     public Czajnik mCzajnik;
-    private float angleVX, angleVY, angleVZ, elevation=0;
+    private float angleVX=0, angleVY=0, angleVZ=0, elevation=0;
 
 
     @Override
@@ -45,43 +43,30 @@ public class arRenderer implements GLSurfaceView.Renderer {
         Matrix.perspectiveM(mProjectionMatrix, 0, 53.13f, aspect, Z_NEAR, Z_FAR);
     }
 
-    private float[] rotCameraX = {
-            1, 0, 0, 0,
-            0, (float)Math.cos(angleVX * Math.PI / 180.0),(float) Math.sin(angleVX * Math.PI / 180.0), 0,
-            0, (float)-Math.sin(angleVX * Math.PI / 180.0), (float)Math.cos(angleVX * Math.PI / 180.0), 0,
-            0, 0, 0, 1};
-
-    private float[] rotCameraY = {
-            (float)Math.cos(angleVY * Math.PI / 180.0), 0, (float)-Math.sin(angleVY * Math.PI / 180.0), 0,
-            0, 1, 0, 0,
-            (float)Math.sin(angleVY * Math.PI / 180.0), 0, (float)Math.cos(angleVY * Math.PI / 180.0), 0,
-            0, 0, 0, 1
-    };
-
-    private float[] rotCameraZ = {
-            (float)Math.cos(angleVZ * Math.PI / 180.0), (float)Math.sin(angleVZ * Math.PI / 180.0), 0, 0,
-            (float)-Math.sin(angleVZ * Math.PI / 180.0), (float)Math.cos(angleVZ * Math.PI / 180.0), 0, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 1
-    };
-
     @Override
     public void onDrawFrame(GL10 gl10) {
+        float[] points = MainActivity.Companion.getResultPoints();
+        float dist = calcDist(points[2],points[6], points[3], points[7]);
+        float QRcenterX = (1/((points[3] + points[5])/2f))*1000;
+        float QRcenterY = (1/((points[0] + points[2])/2f))*1000;
+        elevation = (1/dist)*2000;
 
-        elevation = 1/calcDist(MainActivity.Companion.getResultPoints()[2], MainActivity.Companion.getResultPoints()[6], MainActivity.Companion.getResultPoints()[3], MainActivity.Companion.getResultPoints()[7]);
-        System.out.println(elevation);
         GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT | GLES30.GL_DEPTH_BUFFER_BIT);
         GLES30.glEnable(GLES30.GL_DEPTH_TEST);
 
-        // Set the camera position
-        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, elevation*2500, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        // Set the camera position=4
+        Matrix.setLookAtM(mViewMatrix, 0, 0f, 0f,  elevation, -QRcenterX+3, -QRcenterY+2, 0f, 0f, 1.0f, 0.0f);
         // Create a rotation and translation for the cube
         Matrix.setIdentityM(mRotationMatrix, 0);
-        //move the cube up/down and left/right
-        Matrix.translateM(mRotationMatrix, 0, mTransX, mTransY, 0);
 
-        //mangle is how fast, x,y,z which directions it rotates.
-        Matrix.rotateM(mRotationMatrix, 0, mAngle, 0.4f, 1.0f, 0.6f);
+        //move the cube up/down and left/right
+        Matrix.translateM(mRotationMatrix, 0, 0, 0, 0);
+        //rotate x
+        Matrix.rotateM(mRotationMatrix, 0, angleVX, 1.0f, 0,0);
+        //rotate y
+        Matrix.rotateM(mRotationMatrix, 0, angleVY, 0, 1.0f, 0);
+        //rotate z
+        Matrix.rotateM(mRotationMatrix, 0, angleVZ, 0, 0, 1.0f);
 
         // combine the model with the view matrix
         Matrix.multiplyMM(mMVPMatrix, 0, mViewMatrix, 0, mRotationMatrix, 0);
